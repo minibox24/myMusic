1b83e07db91a704af7f3007838e44629dab74686
minibox24 <minibox724@gmail.com>
2023-03-28 16:35:32 +0900

    downloader

---
+++ downloader/main.py
@@ -0,0 +1,40 @@
+from yt_dlp import YoutubeDL
+from ytmusicapi import YTMusic
+import json
+import os
+
+PLAYLIST = os.getenv("PLAYLIST")
+
+if not os.path.exists("./sources"):
+    os.mkdir("./sources")
+
+ytmusic = YTMusic("headers_auth.json")
+ytdl = YoutubeDL(
+    {
+        "format": "bestaudio",
+        "outtmpl": f"./sources/%(id)s.%(ext)s",
+        "quiet": True,
+        "cookiefile": "cookies.txt",
+    }
+)
+
+playlist = ytmusic.get_playlist(PLAYLIST, limit=1000)
+for track in playlist["tracks"]:
+    data = {
+        "filename": None,
+        "id": track["videoId"],
+        "title": track["title"],
+        "artists": [artist["name"] for artist in track["artists"]],
+        "album": track["album"]["name"],
+        "thumbnail": track["thumbnails"][-1]["url"].split("=")[0],
+    }
+
+    ytdl.download([f"https://www.youtube.com/watch?v={data['id']}"])
+
+    for file in os.listdir("./sources"):
+        if file.startswith(data["id"]):
+            data["filename"] = file
+            break
+
+    with open(f"./sources/{data['id']}.json", "w", encoding="utf8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
